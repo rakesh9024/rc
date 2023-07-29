04166e732409ea2548afd3bdb0ae07e08e599dbd
rakesh9024 <140912484+rakesh9024@users.noreply.github.com>
2023-07-29 23:12:59 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+from kivymd.app import MDApp
+from kivymd.uix.label import MDLabel
+class Main__App(MDApp):
+    def build(self):
+        return MDLabel(text="Rakesh Jaat",halign="center")
+if __name__ == '__main__':
+    Main__App().run()
+
+
+
+
+
